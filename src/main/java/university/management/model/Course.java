eb929836349f03fe8c71f431af4ed19277c35292
Timur Celikel <timur.celikel@proton.me>
2023-08-24 11:20:12 -0700

    Task: Display University Courses on Homepage

--- src/main/java/university/management/model/Course.java
+++ src/main/java/university/management/model/Course.java
@@ -3,43 +3,52 @@ package university.management.model;
 import javax.persistence.*;
 
 /**
- * @author Michael Pogrebinsky - www.topdeveloperacademy.com
- * Persistence Entity that represents a course at the university as well as a row in the Courses
- * table in the database
+ * @author Michael Pogrebinsky - www.topdeveloperacademy.com Persistence Entity that represents a course at the university as well as a row in the
+ * 		Courses table in the database
  */
 
+@Table(name = "Courses")
 @Entity
 public class Course {
 
-    @Id
-    private Long courseId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long courseId;
 
-    private String title;
-    private String description;
-    private int numberOfCredits;
+	private String title;
 
-    public Course() {
-    }
+	private String description;
 
-    public Course(String title, String description, int numberOfCredits) {
-        this.title = title;
-        this.description = description;
-        this.numberOfCredits = numberOfCredits;
-    }
+	private int numberOfCredits;
 
-    public int getNumberOfCredits() {
-        return numberOfCredits;
-    }
+	public Course() {
 
-    public Long getCourseId() {
-        return courseId;
-    }
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public Course(String title, String description, int numberOfCredits) {
 
-    public String getDescription() {
-        return description;
-    }
+		this.title = title;
+		this.description = description;
+		this.numberOfCredits = numberOfCredits;
+	}
+
+	public int getNumberOfCredits() {
+
+		return numberOfCredits;
+	}
+
+	public Long getCourseId() {
+
+		return courseId;
+	}
+
+	public String getTitle() {
+
+		return title;
+	}
+
+	public String getDescription() {
+
+		return description;
+	}
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+DELETE FROM COURSES;
+INSERT INTO COURSES (title, description, number_of_credits) VALUES ('Computer Science 101', 'Introduction to the Field Of Computers and Programming', 3);
+INSERT INTO COURSES (title, description, number_of_credits) VALUES ('Algorithms and Data Structures', 'Advanced theoretical course on how to write efficient programs', 5);
+INSERT INTO COURSES (title, description, number_of_credits) VALUES ('Physics 101', 'Introduction to Mechanics and Newton Laws', 2);
+INSERT INTO COURSES (title, description, number_of_credits) VALUES ('Physics 102', 'Introduction to Electromagnetism', 4);
+INSERT INTO COURSES (title, description, number_of_credits) VALUES ('Linear Algebra', 'Introduction to Algebra, Theory and Practice', 5);
+INSERT INTO COURSES (title, description, number_of_credits) VALUES ('Introduction to Calculus', 'Mathematical Study of Continuous Change', 6);
